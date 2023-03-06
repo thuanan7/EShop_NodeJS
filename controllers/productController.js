4f6e79369bc8ba4a79611b2ba6f5b808ea9e813e
Nguyen Thuan An <thuanan7@gmail.com>
2023-03-06 19:04:01 +0700

    Bai19 display product by category

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,9 +5,24 @@ const models = require("../models");
 const productController = {};
 
 productController.show = async (req, res) => {
-    const products = await models.Product.findAll({
-        attributes: ['id', 'name', 'imagePath', 'stars', 'oldPrice', 'price'],
+    const categoryId = parseInt(req.query.category) || 0;
+
+    const categories = await models.Category.findAll({
+        attributes: ['id', 'name', 'imagePath'],
+        include: [{ model: models.Product }],
     });
+    res.locals.categories = categories;
+
+    const option = {
+        attributes: ['id', 'name', 'imagePath', 'stars', 'oldPrice', 'price'],
+        where: {},
+    };
+
+    if (categoryId > 0) {
+        option.where = {categoryId};
+    }
+
+    const products = await models.Product.findAll(option);
     res.locals.products = products;
 
     res.render("product-list");
