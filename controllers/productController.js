17ac5a1d338b109ddfc905a6160fbc5b2fc57c38
Nguyen Thuan An <thuanan7@gmail.com>
2023-03-06 19:19:57 +0700

    Bai 20 Display product by brand

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,12 +6,19 @@ const productController = {};
 
 productController.show = async (req, res) => {
     const categoryId = parseInt(req.query.category) || 0;
+    const brandId = parseInt(req.query.brand) || 0;
 
     const categories = await models.Category.findAll({
         attributes: ['id', 'name', 'imagePath'],
         include: [{ model: models.Product }],
     });
     res.locals.categories = categories;
+
+    const brands = await models.Brand.findAll({
+        attributes: ['id', 'name', 'imagePath'],
+        include: [{ model: models.Product }],
+    });
+    res.locals.brands = brands;
 
     const option = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'oldPrice', 'price'],
@@ -20,6 +27,10 @@ productController.show = async (req, res) => {
 
     if (categoryId > 0) {
         option.where = {categoryId};
+    }
+
+    if (brandId > 0) {
+        option.where = {brandId};
     }
 
     const products = await models.Product.findAll(option);
