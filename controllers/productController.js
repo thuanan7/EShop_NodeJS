8780403369a727fed5b4ad5a22dacd2b2f8d31df
Nguyen Thuan An <thuanan7@gmail.com>
2023-03-07 15:52:30 +0700

    create productController.getData for pass query things to show and detail page

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,97 +4,85 @@ const models = require("../models");
 
 const productController = {};
 
+productController.getData = async (req, res, next) => {
+	const categories = await models.Category.findAll({
+		attributes: ["id", "name", "imagePath"],
+		include: [{ model: models.Product }],
+	});
+	res.locals.categories = categories;
+
+	const brands = await models.Brand.findAll({
+		attributes: ["id", "name", "imagePath"],
+		include: [{ model: models.Product }],
+	});
+	res.locals.brands = brands;
+
+	const tags = await models.Tag.findAll({
+		attributes: ["id", "name"],
+	});
+	res.locals.tags = tags;
+    next();
+};
+
 productController.show = async (req, res) => {
-    const categoryId = parseInt(req.query.category) || 0;
-    const brandId = parseInt(req.query.brand) || 0;
-    const tagId = parseInt(req.query.tag) || 0;
+	const categoryId = parseInt(req.query.category) || 0;
+	const brandId = parseInt(req.query.brand) || 0;
+	const tagId = parseInt(req.query.tag) || 0;
 
-    const categories = await models.Category.findAll({
-        attributes: ['id', 'name', 'imagePath'],
-        include: [{ model: models.Product }],
-    });
-    res.locals.categories = categories;
+	const option = {
+		attributes: ["id", "name", "imagePath", "stars", "oldPrice", "price"],
+		where: {},
+		include: [],
+	};
 
-    const brands = await models.Brand.findAll({
-        attributes: ['id', 'name', 'imagePath'],
-        include: [{ model: models.Product }],
-    });
-    res.locals.brands = brands;
+	if (categoryId > 0) {
+		option.where = { categoryId };
+	}
 
-    const tags = await models.Tag.findAll({
-        attributes: ['id', 'name'],
-    });
-    res.locals.tags = tags;
+	if (brandId > 0) {
+		option.where = { brandId };
+	}
 
-    const option = {
-        attributes: ['id', 'name', 'imagePath', 'stars', 'oldPrice', 'price'],
-        where: {},
-        include: [],
-    };
+	if (tagId > 0) {
+		option.include = [
+			{
+				model: models.Tag,
+				where: { id: tagId },
+				attributes: [],
+			},
+		];
+	}
 
-    if (categoryId > 0) {
-        option.where = {categoryId};
-    }
+	const products = await models.Product.findAll(option);
+	res.locals.products = products;
 
-    if (brandId > 0) {
-        option.where = {brandId};
-    }
-
-    if (tagId > 0) {
-        option.include = [{
-            model: models.Tag,
-            where: {id: tagId},
-            attributes: []
-        }];
-    }
-
-    const products = await models.Product.findAll(option);
-    res.locals.products = products;
-
-    res.render("product-list");
+	res.render("product-list");
 };
 
 productController.detail = async (req, res) => {
-    const productId = parseInt(req.params.id) || 0;
+	const productId = parseInt(req.params.id) || 0;
 
-    const categories = await models.Category.findAll({
-        attributes: ['id', 'name', 'imagePath'],
-        include: [{ model: models.Product }],
-    });
-    res.locals.categories = categories;
+	const product = await models.Product.findOne({
+		where: { id: productId },
+		include: [
+			{
+				model: models.Image,
+				attributes: ["name", "imagePath"],
+			},
+			{
+				model: models.Review,
+				include: [
+					{
+						model: models.User,
+						attributes: ["firstName", "lastName"],
+					},
+				],
+				attributes: ["review", "stars", "updatedAt"],
+			},
+		],
+	});
 
-    const brands = await models.Brand.findAll({
-        attributes: ['id', 'name', 'imagePath'],
-        include: [{ model: models.Product }],
-    });
-    res.locals.brands = brands;
-
-    const tags = await models.Tag.findAll({
-        attributes: ['id', 'name'],
-    });
-    res.locals.tags = tags;
-
-    const product = await models.Product.findOne({
-        where: {id: productId},
-        include: [
-        { 
-            model: models.Image,
-            attributes: ['name', 'imagePath'] 
-        },
-        {
-            model: models.Review,
-            include: [
-                {
-                    model: models.User,
-                    attributes: ['firstName', 'lastName'],
-                },
-            ],
-            attributes: ['review', 'stars', 'updatedAt'],
-        },
-    ],
-    });
-
-    res.render('product-detail', {product});
+	res.render("product-detail", { product });
 };
 
 module.exports = productController;
